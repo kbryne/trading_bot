fe1ce74196f78f557e6bbe5e8407b413a0d8d2ac
kbryne <kristoffer.bryne@gmail.com>
2024-07-09 18:27:30 +0200

    testing methods in yfinance

--- fetch_data.py
+++ fetch_data.py
@@ -12,8 +12,25 @@ metadata = norsk_hydro.history_metadata
 div_n_splits =norsk_hydro.actions
 div = norsk_hydro.dividends
 splits = norsk_hydro.splits
+income_statement = norsk_hydro.income_stmt
+quarterly_income_statement = norsk_hydro.quarterly_income_stmt
+balance = norsk_hydro.balance_sheet
+quarterly_balance = norsk_hydro.quarterly_balance_sheet
+cashflow = norsk_hydro.cashflow
+quarterly_cashflow = norsk_hydro.quarterly_cashflow
+share_holders = norsk_hydro.major_holders
+mutual_fund_holders = norsk_hydro.mutualfund_holders
+insider_trades = norsk_hydro.insider_purchases
+insider_transactions = norsk_hydro.insider_transactions
+insiders = norsk_hydro.insider_roster_holders
+recommendations = norsk_hydro.recommendations
+recommendations_sum = norsk_hydro.recommendations_summary
+upgrades_downgrades = norsk_hydro.upgrades_downgrades
+earnings_dates = norsk_hydro.earnings_dates
+isin = norsk_hydro.isin
+print(isin)
+
 # share count
 share_count = norsk_hydro.get_shares_full(start="1900-01-01", end="2100-01-01")
-print(share_count)
 
 
